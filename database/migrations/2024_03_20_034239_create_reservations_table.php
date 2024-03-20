696e256033146563a5bdab846bddff2bee433bbe
RansomBroker <yadisoke@gmail.com>
2024-03-20 10:57:32 +0700

    membuat migration table reservations

---
+++ database/migrations/2024_03_20_034239_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->string('name');
+            $table->string('identity');
+            $table->string('identity_img');
+            $table->text('request');
+            $table->string('payment_type');
+            $table->decimal('total', 20, 9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
