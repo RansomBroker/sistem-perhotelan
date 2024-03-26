befd2544fae38fe6cf6f1843af04f93631549d4a
RansomBroker <yadisoke@gmail.com>
2024-03-26 12:56:03 +0700

    membuat sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,1 +1,18 @@
-<?php
+<aside class="min-h-0 flex-1 flex overflow-hidden">
+    <nav aria-label="Sidebar" class="sidebar w-64 lg:block lg:flex-shrink-0 lg:bg-white shadow-lg lg:overflow-y-auto p-4 lg:p-8">
+        <p class="font-bold text-zinc-950 tracking-widest">Sistem Manajemen Dangau</p>
+        <div class="py-8">
+            <a href="" class="flex items-center text-slate-900 mb-6 text-sm"><i class='bx bxs-dashboard me-3' ></i>Dashboard</a>
+            {{-- section 1--}}
+            <p class="mb-6 text-slate-400 uppercase text-xs">Manajemen Kantor</p>
+            <a href="" class="flex items-center text-slate-800 mb-6 text-sm"><i class='bx bxs-building me-3'></i> Kelola Kantor</a>
+            <a href="" class="flex items-center text-slate-800 mb-6 text-sm"><i class='bx bxs-badge me-3' ></i> Kelola Jabatan</a>
+            <a href="" class="flex items-center text-slate-800 mb-6 text-sm"><i class='bx bxs-user-account me-3'></i>Kelola Karyawan</a>
+            {{-- section 2 --}}
+        </div>
+        {{-- Sidebar button--}}
+        <div class="w-full flex justify-center">
+            <button class="sidebar-btn text-4xl"><i class='bx bx-chevron-left-circle text-slate-800'></i></button>
+        </div>
+    </nav>
+</aside>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,10 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 </head>
-<body class="flex min-h-screen min-w-screen">
+<body class="flex min-h-screen min-w-screen items-stretch">
 
     @include('includes.sidebar')
+
+    <script src="{{ asset('src/js/jquery-3.7.1.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('.sidebar-btn').click(function() {
+               if ($('.sidebar').hasClass('active')) {
+                   $('.sidebar').removeClass('active')
+               } else {
+                   $('.sidebar').addClass('active')
+               }
+            });
+        })
+    </script>
 </body>
 </html>
